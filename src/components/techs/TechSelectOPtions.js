dcd26c0e71ba6935d8aefba1dab41b312a3f0465
Purshottam Patidar <parsuboy666@gmail.com>
2020-12-20 17:20:33 +0530

    my project

---
+++ src/components/techs/TechSelectOPtions.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getTechs } from '../../actions/techActions'
+
+const TechSelectOPtions = ({ getTechs , tech: { techs, loading}}) => {
+    
+    useEffect(() => {
+        getTechs();
+        // eslint-disable-next-line
+    }, []);
+    
+    return(
+    
+      !loading && 
+      techs !== null &&
+      techs.map(t => (
+      <option key={t.id} value={`${t.firstName} ${t.lastName}`}>
+        {t.firstName} {t.lastName}
+      </option>))
+
+    );
+};
+
+TechSelectOPtions.propTypes = {
+   tech: PropTypes.object.isRequired,
+   getTechs: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    tech: state.tech
+});
+
+export default connect(mapStateToProps, { getTechs })(TechSelectOPtions);
